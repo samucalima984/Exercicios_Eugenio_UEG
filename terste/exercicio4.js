d89e669f08b29b2cf4abfcc78d689c225ed30da9
SAMUEL DE LIMA COSTA <samucalima984@gmail.com>
2025-08-28 19:28:24 -0300

    e

--- terste/exercicio4.js
+++
@@ -1,4 +0,0 @@
-const base = 50;
-const altura = 10;
-let volume = 1/3*altura * base;
-console.log("Volume = " + volume);
--- terste/exercico2.js
+++
@@ -1,5 +0,0 @@
-const a = 2;
-const b = 8;
-const c = 4;
-let delta = Math.pow(b,2)-4*a*c;
-console.log(delta);
