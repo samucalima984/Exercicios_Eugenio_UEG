954666665db09bfbb9ec118688f28c0a0fda390d
SAMUEL DE LIMA COSTA <samucalima984@gmail.com>
2025-09-09 19:52:42 -0300

    manutencao3

--- Lista6/exercicio5.js
+++ Lista6/exercicio5.js
@@ -1,48 +1,57 @@
-const areaAcres = 100;
-let tipoPulverizacao = 2;
-let strPulverizacao = "";
+let nomeUsuario = "Pedro";
+let tipoPulverizacao = 1;   
+const area = 300;
+let precoPorAcre = 0;
+let strTipoPulverizacao = "";
 
-switch(tipoPulverizacao){
-   case 1:
-      strPulverizacao = "Pulverização contra ervas daninhas";
-      valorTotal = 50 * areaAcres;
-      if(areaAcres > 300){
-         valorTotal = valorTotal - (valorTotal * 0.05);
-      } else if(valorTotal > 10750){
-            valorTotal = valorTotal - (valorTotal * 0.10);
-      } break;
 
-   case 2:
-      strPulverizacao = "Pulverização contra gafanhotos";
-      valorTotal = 100 * areaAcres;
-      if(areaAcres > 300){
-         valorTotal = valorTotal - (valorTotal * 0.05);
-      } else if(valorTotal > 10750){
-            valorTotal = valorTotal - (valorTotal * 0.10);
-      } break;
+switch (tipoPulverizacao) {
+    case 1:
+        strTipoPulverizacao = "Pulverização contra ervas daninhas.";      
+        precoPorAcre = 50.00;
+        break;
 
-   case 3:
-      strPulverizacao = "Pulverização contra broca";
-      valorTotal = 150 * areaAcres;
-      if(areaAcres > 300){
-         valorTotal = valorTotal - (valorTotal * 0.05);
-      } else if(valorTotal > 10750){
-            valorTotal = valorTotal - (valorTotal * 0.10);
-      } break;
+    case 2:
+        strTipoPulverizacao = "Pulverização contra gafanhotos.";
+        precoPorAcre = 100.00;
+        break;
 
-   case 4:
-      strPulverizacao = "Pulverização contra tudo";
-      valorTotal = 250 * areaAcres;
-     if(areaAcres > 300){
-         valorTotal = valorTotal - (valorTotal * 0.05);
-      } else if(valorTotal > 10750){
-            valorTotal = valorTotal - (valorTotal * 0.10);
-      } break;
+    case 3:
+        strTipoPulverizacao = "Pulverização contra broca.";
+        precoPorAcre = 150.00;
+        break;
 
-   default:
-      strPulverizacao = "Tipo de Pulverização inválido";
-      valorTotal = 0;
+    case 4:
+        strTipoPulverizacao = "Pulverização contra tudo.";
+        precoPorAcre = 250.00;
+        break;
+        
+    default:
+        console.log("Tipo de pulverização inválido!");
 }
-   const saida = "Tipo da Pulverização: " + strPulverizacao;
-   const saida2 = "Valor da pulverizacao: R$" + valorTotal;
-   console.log(saida + "\n" + saida2);
+
+custo = precoPorAcre * area;
+
+
+switch (true) {
+    case (area > 300):
+        custo = custo + (custo * 0.05);
+        break;
+    default:
+        
+        break;
+}
+
+switch (true) {
+    case (custo > 10750):
+        let excedente = custo - 10750;
+        custo = 10750 + (excedente * 0.1);
+        break;
+    default:
+        
+        break;
+}
+
+console.log("Usuário: " + nomeUsuario);
+console.log("Tipo de Pulverização: " + strTipoPulverizacao);
+console.log("Valor a ser pago: R$ " + custo.toFixed(2));
