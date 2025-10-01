1794bb684800d9b4371099a66da18a71f375e281
SAMUEL DE LIMA COSTA <samucalima984@gmail.com>
2025-10-01 19:39:55 -0300

    atualizados 2.1

--- html/Estruturas de Repetição/Lista1/Divisores de Um Número Inteiro Positivo/js/arquivo1.js
+++ html/Estruturas de Repetição/Lista1/Divisores de Um Número Inteiro Positivo/js/arquivo1.js
@@ -9,7 +9,7 @@ function gerarDivisores(numero){
   }
     let saida = "";
     for (let i = 1; i <= n; i++) {
-        if (n % i === 0) {
+        if (n % i == 0) {
             saida += numero + " / " + i + " = " + i * numero + "\n";
         }
     }
